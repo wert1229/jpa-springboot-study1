b8d545081756521626aee9348879e3107d81bc67
kdpark <wert1229@gmail.com>
2020-07-25 23:59:45 +0900

    Section1

---
+++ src/test/java/com/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.jpabook.jpashop;
+
+import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("A");
+
+        //when
+        Long id = memberRepository.save(member);
+        Member findMember = memberRepository.find(id);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
