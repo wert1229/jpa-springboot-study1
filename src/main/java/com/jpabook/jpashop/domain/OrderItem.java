9dbe54cb4aa0352a2e8100fef7648f99dbecfb54
kdpark <wert1229@gmail.com>
2020-07-26 22:42:29 +0900

    Section2

---
+++ src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,28 @@
+package com.jpabook.jpashop.domain;
+
+import com.jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int orderPrice;
+
+    private int count;
+}
